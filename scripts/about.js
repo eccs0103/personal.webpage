ef9ae1ece9eed2373bd8c7263081b03796bc64bc
eccs0103 <eccs0103@gmail.com>
2023-04-06 00:25:11 +0400

    1.0.0

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,24 @@
 "use strict";
 try {
-	// Your code goes here
+	const birthday = new Date(`2004-03-24 12:00`);
+
+	const timeBirthday = (/** @type {HTMLTimeElement} */ (document.querySelector(`time.-birthday`)));
+	timeBirthday.dateTime = birthday.toString();
+	timeBirthday.innerText = `${birthday.toLocaleString(undefined, {
+		year: `numeric`,
+		month: `numeric`,
+		day: `numeric`,
+		hourCycle: `h24`,
+		hour: `2-digit`,
+		minute: `2-digit`,
+	})}`;
+
+	const spanAge = (/** @type {HTMLSpanElement} */ (document.querySelector(`span.-age`)));
+	spanAge.innerText = `${Math.round((Date.now() - birthday.valueOf()) / 1000 / 60 / 60 / 24 / 365)} years`;
+	setInterval(() => {
+		spanAge.innerText = `${Math.round((Date.now() - birthday.valueOf()) / 1000 / 60 / 60 / 24 / 365)} years`;
+	}, 1000);
+
 } catch (exception) {
 	Application.prevent(exception);
 }
